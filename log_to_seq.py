cbc55fda406535593899a4e5233b5373f4e8a9f1
Bartosz Kalinczuk <bartosz.kalinczuk@nsn.com>
2014-09-16 15:34:04 +0200

    log_to_seq: some options implemented

--- log_to_seq.py
+++ log_to_seq.py
@@ -5,61 +5,168 @@ import re
 def parse_commandline_options(): 
     from optparse import OptionParser 
     parser = OptionParser() 
-    #parser.add_option("-s", "--stack", action='store_true', dest="stack", help="produces stack trace for each running component") 
-    #parser.add_option("-l", "--last_line", type='int', dest="line", help="prints n last lines for each running component") 
-    return parser.parse_args() 
+    parser.add_option("", "--debug_logs", default=True, action='store_false', dest="no_debug", help="turn to produce debug logs") 
+    parser.add_option("", "--no_ulogs", action='store_true', help="disable ulog") 
+    parser.add_option("", "--since_line", type='str', dest="since_line", help="the generated logs will start from there")
+    parser.add_option("", "--till_line", type='str', dest="till_line", help="the generated logs will end here")
+    parser.add_option("", "--disable_msgs", type='str', dest="disabled_msgs", help="comma separated list of messages to not display in output diagram")
+    parser.add_option("", "--external_only", action='store_true', help="only communication with system will be inserted") 
+    (options, args) = parser.parse_args() 
+    if (options.disabled_msgs):
+        options.disabled_msgs = options.disabled_msgs.split(',')
+    return (options, args)
 
 def components(log):
     cocrs = [re.search('\|cocr\|.*\|(.*)\|(once|alive)', line) for line in log]
     return set([res.group(1) for res in cocrs if res != None])
 
-def messages(comps, log):
+def writeCommOp(out, msg, receiver, sender):
+    out.write(sender + " -> " + receiver + ": " + msg + "\n")
+
+class Event:
+    def equals(self, other):
+        return False if (self.__class__ != other.__class__) else self.equals_impl(other)
+
+class CommEvent(Event):
+    def __init__(self):
+        self.msg = "invalid"
+        self.receiver = "invalid"
+        self.sender = "invalid"
+    
+    def filter(self, options):
+        externals = ["system", "null"]
+        ret = True
+        if (options.external_only):
+            ret = ret and (self.receiver in externals) or (self.sender in externals)
+        if (options.disabled_msgs):
+            print (self.msg, options.disabled_msgs, (self.msg in options.disabled_msgs))
+            if (self.msg in options.disabled_msgs):
+                ret = False
+        return ret
+    def produce(self, out):
+        writeCommOp(out, self.msg, self.receiver, self.sender)
+    def equals_impl(self, other):
+        return (self.msg == other.msg) and (self.receiver == other.receiver) and (self.sender == other.sender)
+
+class PtrxEvent(CommEvent):
+    def __init__(self, line):
+        CommEvent.__init__(self)
+        self.extractInfo(line)
+
+    def extractInfo(self, line):
+        res = re.search('\|ptrx\|([^=]+)=.*value=[^.]+\.([^:]+).*sender=[^:]*:(.*),timestamp=.*consume', line)
+        if (res != None):
+            self.msg = res.group(2)
+            self.receiver = res.group(1)
+            self.sender = res.group(3)
+        res = re.search("(.*)\(running\)", self.sender)
+        if (res != None):
+            self.sender = res.group(1)
+    
+    def update(self, comps):
+        if not (self.sender in comps) and (self.sender != "null"):
+            self.sender = "system"
+        
+
+class PtsdEvent(CommEvent):
+    def __init__(self, line):
+        CommEvent.__init__(self)
+        self.extractInfo(line)
+
+    def extractInfo(self, line):
+        res = re.search('\|ptsd\|([^=]+)=.*\|SYSTEM\.[^|]+\|[^.]+\.([^|]+)\|', line)
+        if (res != None):
+            self.msg = res.group(2)
+            self.receiver = "system"
+            self.sender = res.group(1)
+    def update(self, comps):
+        pass
+
+class UlogEvent(Event):
+    def __init__(self, line):
+        res = re.search('\|ulog\|[^|]+\|"([A-Z]*): *""([^"]*)".*', line)
+        self.level = res.group(1)
+        self.msg = res.group(2)
+    def update(self, comps):
+        pass
+    def filter(self, options):
+        return (not options.no_debug) or ((self.level != "DEBUG") and (not options.no_ulogs))
+    def produce(self, out):
+        out.write("== " + self.level + " " + self.msg + " ==\n")
+    def equals_impl(self, other):
+        return False
+        #return (self.level == other.level) and (self.msg == other.msg)
+
+class MultipleEvents(Event):
+    def __init__(self, event, times):
+        self.event = event
+        self.times = times
+    def update(self, comps):
+        pass
+    def produce(self, out):
+        out.write("loop " + str(self.times) + " times\n")
+        self.event.produce(out)
+        out.write("end\n")
+
+def createEvents(log):
+    def is_ulog(line):
+        return re.match('[0-9.T]+\|ulog\|', line) != None
     def is_ptsd(line):
         return re.match('[0-9.T]+\|ptsd\|.*SYSTE', line) != None
     def is_ptrx(line):
         return re.match('[0-9.T]+\|ptrx\|.*consume.*', line) != None
-    def extractRecvInfo(line):
-        res = re.search('\|ptrx\|([^=]+)=.*value=[^.]+\.([^:]+).*sender=[^:]*:(.*),timestamp=.*consume', line)
-        if (res == None):
-            print(line)
-            return ("invalid", "invalid", "invalid")
-        return (res.group(2), res.group(1), res.group(3))
-    def extractSendInfo(line):
-        res = re.search('\|ptsd\|([^=]+)=.*\|SYSTEM\.[^|]+\|[^.]+\.([^|]+)\|', line)
-        if (res == None):
-            print(line)
-            return ("invalid", "invalid", "invalid")
-        return (res.group(2), "system", res.group(1))
+    def is_intresting(line):
+        return is_ptsd(line) or is_ptrx(line) or is_ulog(line)
     def extractInfo(line):
         if (is_ptsd(line)):
-            return extractSendInfo(line)
-        return extractRecvInfo(line)
-    return [extractInfo(line) for line in log if (is_ptsd(line) or is_ptrx(line))]
+            return PtsdEvent(line)
+        if (is_ulog(line)):
+            return UlogEvent(line)
+        return PtrxEvent(line)
+    return [extractInfo(line) for line in log if is_intresting(line)]
 
-def updateSutMsgs(comps, msgs):
-    def convert((msg, receiver, sender)):
-        if (sender == "null"):
-            return (msg, receiver, sender)
-        res = re.search("(.*)\(running\)", sender)
-        if (res != None):
-            sender = res.group(1)
-        return (msg, receiver, sender) if (sender in comps) else (msg, receiver, "system")
-    return [convert(msg) for msg in msgs]
+def contract(events):
+    def create_multiple_or_not(times, first):
+        return first if (times == 1) else MultipleEvents(first, times)
+    def contract_two((times, last, acc), event):
+        if (last == None):
+            return (1, event, acc)
+        elif (last.equals(event)):
+            return (times + 1, last, acc)
+        else:
+            return (1, event, acc + [create_multiple_or_not(times, last)])
+    (times, last, events) = reduce(contract_two, events, (0, None, []))
+    if (last == None):
+        return []
+    return events + [create_multiple_or_not(times, last)]
 
-def generate_uml(logfile):
-    log = open(logfile).read().split("\n")
+def generate_uml(logfile, options):
+    since_log_seen = (options.since_line == None)
+    log = []
+    for line in open(logfile).read().split("\n"):
+        if (options.since_line and not since_log_seen):
+            if (re.match(".*" + options.since_line, line)):
+                since_log_seen = True
+        if (options.till_line and since_log_seen):
+            if (re.match(".*" + options.till_line, line)):
+                break
+        if (since_log_seen):
+            log.append(line)
     comps = components(log)
-    msgs = messages(comps, log)
-    msgs = updateSutMsgs(comps, msgs)
+    events = createEvents(log)
+    for event in events:
+        event.update(comps)
+    events = filter(lambda e : e.filter(options), events)
+    events = contract(events)
     out = open(logfile + ".uml", "w")
-    out.write("@startuml")
-    for (msg, receiver, sender) in msgs:
-        out.write(sender + " -> " + receiver + ": " + msg + "\n")
-    out.write("@enduml")
+    out.write("@startuml\n")
+    for event in events:
+        event.produce(out)
+    out.write("@enduml\n")
 
 if __name__ == "__main__":
     (options, args) = parse_commandline_options() 
     for logfile in args:
-        generate_uml(logfile)
+        generate_uml(logfile, options)
 
 
