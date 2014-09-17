c03f0755bf9622eeb9e554b3383d95165395fa26
Bartosz Kalinczuk <bartosz.kalinczuk@nsn.com>
2014-09-17 11:42:43 +0200

    refactored, added msg extractor, change comm to use ptqu, fixed some errors

--- log_to_seq.py
+++ log_to_seq.py
@@ -10,26 +10,28 @@ def parse_commandline_options():
     parser.add_option("", "--since_line", type='str', dest="since_line", help="the generated logs will start from there")
     parser.add_option("", "--till_line", type='str', dest="till_line", help="the generated logs will end here")
     parser.add_option("", "--disable_msgs", type='str', dest="disabled_msgs", help="comma separated list of messages to not display in output diagram")
-    parser.add_option("", "--external_only", action='store_true', help="only communication with system will be inserted") 
+    parser.add_option("", "--external_only", action='store_true', help="Disabled - external only is default") 
     (options, args) = parser.parse_args() 
     if (options.disabled_msgs):
         options.disabled_msgs = options.disabled_msgs.split(',')
     return (options, args)
 
-def components(log):
-    cocrs = [re.search('\|cocr\|.*\|(.*)\|(once|alive)', line) for line in log]
-    return set([res.group(1) for res in cocrs if res != None])
 def mapped(log):
     ports = [re.search('\|ptmp\|.*\|([^|]+)\|SYSTEM', line) for line in log]
     return set([res.group(1) for res in ports if res != None])
     
 
 def writeCommOp(out, msg, receiver, sender):
-    out.write(sender + " -> " + receiver + ": " + msg + "\n")
+    def getComponent(port):
+        res = re.match('([^.]+)\.', port)
+        return port if (res == None) else res.group(1)
+    out.write(getComponent(sender) + " -> " + getComponent(receiver) + ": " + msg + "\n")
 
 class Event:
     def equals(self, other):
         return False if (self.__class__ != other.__class__) else self.equals_impl(other)
+    def update(self, systemPorts):
+        pass
 
 class CommEvent(Event):
     def __init__(self):
@@ -37,62 +39,72 @@ class CommEvent(Event):
         self.receiver = "invalid"
         self.sender = "invalid"
     
-    def filter(self, options):
-        externals = ["system", "null"]
+    def filter(self, options, systemPorts):
         ret = True
         if (options.external_only):
-            ret = ret and (self.receiver in externals) or (self.sender in externals)
+            ret = ret and ((self.sender == "system") or (self.receiver == "system"))
         if (options.disabled_msgs):
-            if (self.msg in options.disabled_msgs):
-                ret = False
+            ret = ret and (self.msg in options.disabled_msgs)
         return ret
+
     def produce(self, out):
         writeCommOp(out, self.msg, self.receiver, self.sender)
     def equals_impl(self, other):
         return (self.msg == other.msg) and (self.receiver == other.receiver) and (self.sender == other.sender)
 
-class PtrxEvent(CommEvent):
-    def __init__(self, line):
+#most communication can be extracted through ptqu event
+class PtquEvent(CommEvent):
+    def __init__(self, line, msgExtractor):
         CommEvent.__init__(self)
-        self.extractInfo(line)
+        self.extractInfo(line, msgExtractor)
 
-    def extractInfo(self, line):
-        res = re.search('\|ptrx\|([^=]+)=.*value=[^.]+\.([^:]+).*sender=[^:]*:(.*),timestamp=.*consume', line)
+    def extractInfo(self, line, msgExtractor):
+        res = re.search('\|ptqu\|([^=]+)=[^|]+\|([^|]+)\|message\(value=[^.]+\.([^:]+)(.*)\)', line)
         if (res != None):
-            self.msg = res.group(2)
-            self.receiver = res.group(1)
-            self.sender = res.group(3)
-        res = re.search("(.*)\(running\)", self.sender)
-        if (res != None):
+            self.msg = msgExtractor(res.group(3), res.group(4))
+            self.receiver = res.group(2)
             self.sender = res.group(1)
+        else:
+            print(line)
     
-    def update(self, comps, systemPorts):
-        if not (self.sender in comps) and (self.sender != "null"):
+    def update(self, systemPorts):
+        if (self.sender == '?'):
             self.sender = "system"
+        #if not (self.sender in systemPorts):
+            #self.sender = "system"
+
+    @staticmethod
+    def descr():
+        return "ptqu"
         
 
+#sending to system - can be extracted only through ptsd event
 class PtsdEvent(CommEvent):
-    def __init__(self, line):
+    def __init__(self, line, msgExtractor):
         CommEvent.__init__(self)
-        self.extractInfo(line)
+        self.extractInfo(line, msgExtractor)
 
-    def extractInfo(self, line):
-        res = re.search('\|ptsd\|([^=]+)=[^|]+\|[^|]+\|([^|:]+):[^|]+\|[^.]+\.([^|]+)\|', line)
+    def extractInfo(self, line, msgExtractor):
+        res = re.search('\|ptsd\|[^|]+\|([^|]+)\|[^|]+\|[^.]+\.([^|]+)\|(.*)', line)
         if (res != None):
-            self.msg = res.group(3)
-            self.receiver = res.group(2)
+            self.msg = msgExtractor(res.group(2), res.group(3))
+            self.receiver = "invalid"
             self.sender = res.group(1)
             if (len(self.receiver) > len("SYSTEM")) and (self.receiver[:len("SYSTEM")] == "SYSTEM"):
                 self.receiver = "system"
-    def filter(self, options):
+
+    def filter(self, options, systemPorts):
         return self.receiver == "system"
-    def update(self, comps, systemPorts):
-        if (self.receiver in systemPorts):
-            print (self.receiver)
+    def update(self, systemPorts):
+        if (self.sender in systemPorts):
             self.receiver = "system"
 
+    @staticmethod
+    def descr():
+        return "ptsd"
+
 class UlogEvent(Event):
-    def __init__(self, line):
+    def __init__(self, line, _):
         res = re.search('\|ulog\|[^|]+\|("([A-Z]*): *""([^"]*)".*)', line)
         if (res == None):
             res = re.search('\|ulog\|[^|]+\|(.*)', line)
@@ -101,42 +113,41 @@ class UlogEvent(Event):
         else:
             self.level = res.group(2)
             self.msg = res.group(3)
-    def update(self, comps, systemPorts):
-        pass
-    def filter(self, options):
+    def filter(self, options, systemPorts):
         return (not options.no_debug) or ((self.level != "DEBUG") and (not options.no_ulogs))
     def produce(self, out):
         out.write("== " + self.level + " " + self.msg + " ==\n")
     def equals_impl(self, other):
         return (self.level == other.level) and (self.msg == other.msg)
 
+    @staticmethod
+    def descr():
+        return "ulog"
+
 class MultipleEvents(Event):
     def __init__(self, event, times):
         self.event = event
         self.times = times
-    def update(self, comps, systemPorts):
-        pass
     def produce(self, out):
         out.write("loop " + str(self.times) + " times\n")
         self.event.produce(out)
         out.write("end\n")
 
-def createEvents(log):
-    def is_ulog(line):
-        return re.match('[0-9.T]+\|ulog\|', line) != None
-    def is_ptsd(line):
-        return re.match('[0-9.T]+\|ptsd\|', line) != None
-    def is_ptrx(line):
-        return re.match('[0-9.T]+\|ptrx\|.*consume.*', line) != None
-    def is_intresting(line):
-        return is_ptsd(line) or is_ptrx(line) or is_ulog(line)
+class Invalid(Event):
+    def __init__(_1, _2, _3):
+        pass
+
+def createEvents(log, msgExtractor):
+    eventTypes = [UlogEvent, PtsdEvent, PtquEvent]
+
+    def is_event(eventType, line):
+        return re.match('[0-9.T]+\|' + eventType.descr() + '\|', line) != None
+    def is_interesting(line):
+        return any(map(lambda eventType : is_event(eventType, line), eventTypes))
     def extractInfo(line):
-        if (is_ptsd(line)):
-            return PtsdEvent(line)
-        if (is_ulog(line)):
-            return UlogEvent(line)
-        return PtrxEvent(line)
-    return [extractInfo(line) for line in log if is_intresting(line)]
+        eventType = next((eventType for eventType in eventTypes if is_event(eventType, line)), Invalid)
+        return eventType(line, msgExtractor) 
+    return [extractInfo(line) for line in log if is_interesting(line)]
 
 def contract(events):
     def create_multiple_or_not(times, first):
@@ -153,9 +164,22 @@ def contract(events):
         return []
     return events + [create_multiple_or_not(times, last)]
 
+def msgExtractor(msg, line):
+    if (msg == "IMChangeRequest"):
+        res = re.search('request_id:=([^,]+).*distname:="([^"]+)"', line)
+        #res = re.search("object:=.*object:={([^:]+):=", line)
+        if (res == None):
+            return msg
+        return msg + "#" + res.group(1) + "(" + res.group(2) + ")"
+    if (msg == "IMOperationExecuted"):
+        res = re.search('request_id:=([^,]+)', line)
+        if (res == None):
+            return msg
+        return msg + "#" + res.group(1)
+    return msg
+
 def generate_uml(logfile, options):
     fromfile = open(logfile).read().split("\n")
-    comps = components(fromfile)
     systemPorts = mapped(fromfile)
 
     since_log_seen = (options.since_line == None)
@@ -169,10 +193,10 @@ def generate_uml(logfile, options):
                 break
         if (since_log_seen):
             log.append(line)
-    events = createEvents(log)
+    events = createEvents(log, msgExtractor)
     for event in events:
-        event.update(comps, systemPorts)
-    events = filter(lambda e : e.filter(options), events)
+        event.update(systemPorts)
+    events = filter(lambda e : e.filter(options, systemPorts), events)
     events = contract(events)
     out = open(logfile + ".uml", "w")
     out.write("@startuml\n")
