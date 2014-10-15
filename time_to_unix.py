482514ed4fb74da6b59c23ce388977e38631d53a
Bartosz Kalinczuk <bartosz.kalinczuk@nsn.com>
2014-10-15 17:35:12 +0200

    time_to_unix

---
+++ time_to_unix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+def parse_command_options(): 
+    from optparse import OptionParser 
+    parser = OptionParser() 
+    parser.add_option("-f", "--format", help="timestamp format")
+    return parser.parse_args() 
+            
+
+def tounix(timestamp, format):
+    from datetime import datetime
+    from time import mktime
+    date = datetime.strptime(timestamp, format)
+    return mktime(date.timetuple())
+
+
+def main():
+    (options, args) = parse_command_options()
+    for arg in args:
+        print(tounix(arg, options.format))
+
+if __name__ == '__main__':
+    main()
+
